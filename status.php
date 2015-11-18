7f0d434e438174b986a4441fbcb1bcb419727f20
Christian Kohlstedde <christian@kohlsted.de>
2015-11-18 16:17:40 +0100

    wrong check

--- status.php
+++ status.php
@@ -19,15 +19,16 @@ $totalTasks = $totalTasks < 1 ? 1 : $totalTasks;
 echo 'Total tasks ', $totalTasks, "\n";
 
 if ($resultDelayedTasks->num_rows == 1) {
-    $delayedTasks = $resultDelayedTasks->fetch_array()[0];
+    $delayedTasks = (int)$resultDelayedTasks->fetch_array()[0];
 }
+
 $delayedTasks = $delayedTasks < 0 ? 0 : $delayedTasks;
 
 echo 'Delayed tasks ', $delayedTasks, ' | ', number_format($delayedTasks / $totalTasks * 100, 2), "%\n";
 
 if ($resultDelayedTime->num_rows == 1) {
     $result = $resultDelayedTime->fetch_array();
-    $delayedTime = $result[0] != 'NULL' ? (time() - strtotime($result[0])) : 0;
+    $delayedTime = $result[0] != null ? (time() - strtotime($result[0])) : 0;
 }
 
 echo 'Delay time ', $delayedTime <= 0 ? '00:00:00' : date('H:i:s', $delayedTime), "\n";
