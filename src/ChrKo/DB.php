ada92ef2979f309b2e25954292c9fcaecf755077
Christian Kohlstedde <christian@kohlsted.de>
2015-11-19 12:41:21 +0100

    dump failed queries to file

--- src/ChrKo/DB.php
+++ src/ChrKo/DB.php
@@ -42,7 +42,7 @@ class DB extends \mysqli
     {
         $result = parent::query($query, $resultmode);
 
-        if ($this->errno != 0) {
+        if ($this->errno) {
             echo "Query failed\n";
             file_put_contents('failed_queries.sql', $this->error, FILE_APPEND);
             file_put_contents('failed_queries.sql', $this->errno, FILE_APPEND);
