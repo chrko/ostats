7c5dfb25c50cae54be63e7f68fb63984fa26cef6
Christian Kohlstedde <christian@kohlsted.de>
2015-11-23 17:33:59 +0100

    Missing escaping inputs

--- src/ChrKo/Updater.php
+++ src/ChrKo/Updater.php
@@ -23,6 +23,10 @@ abstract class Updater implements UpdaterInterface
 
     public function run($data)
     {
+        foreach ($data as $key => $value) {
+            $data[$key] = $this->db->real_escape_string($value);
+        }
+
         $this->query .= DB::namedReplace($this->getQueryPart(), $data);
         $this->counter++;
 
