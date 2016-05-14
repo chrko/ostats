929299de2cc824d0d7444124827b2af9c37e3ea7
Christian Kohlstedde <christian@kohlsted.de>
2016-05-15 00:59:54 +0200

    rewrite partition command and create units of work

--- src/ChrKo/OStats/Command/PartitionCommand.php
+++ src/ChrKo/OStats/Command/PartitionCommand.php
@@ -18,110 +18,209 @@ class PartitionCommand extends Command {
     }
 
     protected function execute(InputInterface $input, OutputInterface $output) {
-        $steps = [];
+        if (!defined('HIGHSCORE_DAY_PAST')) {
+            define('HIGHSCORE_DAY_PAST', 7);
+        }
 
-        define('RESOLUTION_DAY_COUNT', 7);
+        if (!defined('HIGHSCORE_DAY_FUTURE')) {
+            define('HIGHSCORE_DAY_FUTURE', 4);
+        }
+
+        if (!defined('HIGHSCORE_WEEK_PAST')) {
+            define('HIGHSCORE_WEEK_PAST', 2);
+        }
+
+        $steps = [];
 
         $today = new \DateTime('now', new \DateTimeZone('Etc/UTC'));
         $output->writeln('Today: ' . $today->format('c'));
+        $today->setTime(24, 0, 0);
 
-        $date = clone $today;
+        $oneDay = new \DateInterval('P1D');
 
-        $date->setTime(0, 0, 0);
-        $date->add(new \DateInterval('P1D'));
+        for ($i = -HIGHSCORE_DAY_PAST; $i <= HIGHSCORE_DAY_FUTURE; $i++) {
+            $date = clone $today;
+            if ($i < 0) {
+                $date->sub(new \DateInterval('P' . -$i . 'D'));
+            }
+            else {
+                $date->add(new \DateInterval('P' . $i . 'D'));
+            }
 
-        for ($i = 0; $i < RESOLUTION_DAY_COUNT; $i++) {
             $step = [];
             $step['limit_timestamp'] = $date->getTimestamp();
-            $step['limit_readable'] = date('c', $step['limit_timestamp']);
-
-            $date->sub(new \DateInterval('P1D'));
+            $date->sub($oneDay);
             $step['name'] = 'd_' . $date->format('Ymd');
+            $step['limit_timestamp_low'] = $date->getTimestamp();
 
             $steps[] = $step;
         }
 
-        $minDayLimitTimestamp = $step['limit_timestamp'];
+        $minDayLimitTimestamp = array_reduce(
+            $steps,
+            function ($carry, $item) {
+                if ($carry == NULL) {
+                    return $item['limit_timestamp'];
+                }
+                return min($carry, $item['limit_timestamp']);
+            }
+        );
 
         $weekWanted = clone $today;
+        $weekWanted->setTimestamp($minDayLimitTimestamp);
+        $weekWanted->sub($oneDay);
 
-        $weekWanted->setTime(0, 0, 0);
-        $weekWanted->setISODate($weekWanted->format('Y'), (int) $weekWanted->format('W') + 1, 1);
-
-        for ($i = 0; $i < 2; $i++) {
+        for ($i = 0; $i < HIGHSCORE_WEEK_PAST; $i++) {
             $step = [];
 
-            $weekWanted->sub(new \DateInterval('P' . RESOLUTION_DAY_COUNT . 'D'));
-
             $step['limit_timestamp'] = $weekWanted->getTimestamp();
-
-            $step['limit_timestamp'] =
-                $step['limit_timestamp'] >= $minDayLimitTimestamp
-                    ? $minDayLimitTimestamp - 24 * 60 * 60
-                    : $step['limit_timestamp'];
-
-            $step['limit_readable'] = date('c', $step['limit_timestamp']);
+            $weekWanted->sub($oneDay);
             $step['name'] = 'w_' . $weekWanted->format('YW');
+            $weekWanted->setISODate($weekWanted->format('Y'), (int) $weekWanted->format('W'), 1);
+            $step['limit_timestamp_low'] = $weekWanted->getTimestamp();
 
             $steps[] = $step;
         }
 
-        //$output->write(print_r($steps, TRUE));
+        $maxStepTimestamp = array_reduce(
+            $steps,
+            function ($carry, $item) {
+                if ($carry == NULL) {
+                    return $item['limit_timestamp'];
+                }
+                return max($carry, $item['limit_timestamp']);
+            }
+        );
 
-        $steps = array_reverse($steps);
+        $steps[] = [
+            'name'                => 'pLast',
+            'limit_timestamp'     => $maxSpecialTimestamp = $today->add(new \DateInterval('P1Y'))
+                ->getTimestamp(),
+            'limit_timestamp_low' => $maxStepTimestamp,
+            'max'                 => TRUE
+        ];
+
+        array_walk($steps, function (&$el) {
+            $el['limit_readable'] = date('c', (int) $el['limit_timestamp']);
+            $el['limit_readable_low'] = date('c', (int) $el['limit_timestamp_low']);
+            $el['max'] = $el['max'] ?? FALSE;
+        });
+
+        usort($steps, function ($a, $b) {
+            return $a['limit_timestamp'] <=> $b['limit_timestamp'];
+        });
+
+        $origSteps = $steps;
 
         foreach (XmlApiUpdate::getAllowedArguments()['highscore']['category'] as $category) {
             foreach (XmlApiUpdate::getAllowedArguments()['highscore']['type'] as $type) {
                 $table = 'highscore_' . $category . '_' . $type;
+                $steps = $origSteps;
 
-                foreach ($steps as $step) {
-                    $parts = DB::getConn()
-                        ->query('SELECT PARTITION_NAME AS `name`, PARTITION_DESCRIPTION AS `limit` FROM information_schema.PARTITIONS
+                $parts = DB::getConn()
+                    ->query('SELECT PARTITION_NAME AS `name`, PARTITION_DESCRIPTION AS `limit_timestamp` FROM information_schema.PARTITIONS
                     WHERE TABLE_SCHEMA = \'' . DB_NAME . '\'
                     AND TABLE_NAME LIKE \'' . $table . '\'')
-                        ->fetch_all(MYSQLI_ASSOC);
+                    ->fetch_all(MYSQLI_ASSOC);
 
-                    $step['from'] = [];
+                array_walk($parts, function (&$el) use ($maxSpecialTimestamp) {
+                    if ($el['limit_timestamp'] == 'MAXVALUE') {
+                        $el['limit_timestamp'] = $maxSpecialTimestamp;
+                        $el['max'] = TRUE;
+                    }
+                    $el['limit_timestamp'] = (int) $el['limit_timestamp'];
+                    $el['limit_readable'] = date('c', (int) $el['limit_timestamp']);
+                    $el['max'] = $el['max'] ?? FALSE;
+                });
 
-                    $pLastSkip = FALSE;
-                    foreach ($parts as $part) {
-                        if ($part['name'] == 'p0') {
-                            continue;
-                        }
-                        if ($step['name'] == $part['name'] && $step['limit_timestamp'] == (int) $part['limit']) {
-                            continue 2;
-                        }
-                        if ($pLastSkip || $part['limit'] != 'MAXVALUE') {
-                            if ($step['limit_timestamp'] > (int) $part['limit']
-                                && $step['name'] != $part['name']
-                                && !(strpos($step['name'], 'w_') !== FALSE && strpos($part['name'], 'd_') !== FALSE)
-                            ) {
-                                continue;
-                            }
-                            if ($step['limit_timestamp'] < (int) $part['limit']) {
-                                continue;
-                            }
-                            if ($step['limit_timestamp'] == (int) $part['limit']) {
-                                $pLastSkip = TRUE;
-                            }
-                        }
-                        $step['from'][$part['name']] = [
-                            'name' => $part['name'],
-                            'limit' => $part['limit'],
-                        ];
+                usort($parts, function ($a, $b) {
+                    return $a['limit_timestamp'] <=> $b['limit_timestamp'];
+                });
+
+                $unitsOfWork = [];
+
+                foreach ($parts as $pKey => $part) {
+                    if (count($unitsOfWork) > 0
+                        && $unitsOfWork[$usOWKey = count($unitsOfWork) - 1]['to_max_limit'] >= $part['limit_timestamp']
+                    ) {
+                        $unitsOfWork[$usOWKey]['from'][] = $part;
                         continue;
                     }
+                    $unitOfWork = ['from' => [$part], 'to' => []];
+                    foreach ($steps as $sKey => $step) {
+                        if (!$part['max']
+                            && (
+                                $part['limit_timestamp'] <= $step['limit_timestamp_low']
+                                || $part['limit_timestamp'] > $step['limit_timestamp']
+                            )
+                        ) {
+                            continue;
+                        }
 
-                    $sql = "ALTER TABLE ${table} REORGANIZE PARTITION " . join(',', array_keys($step['from']))
-                        . " INTO (\n";
-                    $sql .= "PARTITION ${step['name']} VALUES LESS THAN (${step['limit_timestamp']})";
-                    if (in_array('pLast', array_keys($step['from']))) {
-                        $sql .= ",\nPARTITION pLast VALUES LESS THAN MAXVALUE";
+                        if (!($part['name'] == $step['name'] && $part['limit_timestamp'] == $step['limit_timestamp'] && !($part['max'] && count($unitOfWork['to']) > 0))) {
+                            $unitOfWork['to'][] = $step;
+                        }
+
+                        unset($steps[$sKey]);
+                        continue;
+                    }
+                    if (count($unitOfWork['to']) > 0) {
+                        $unitOfWork['to_max_limit'] = array_reduce(
+                            $unitOfWork['to'],
+                            function ($carry, $item) {
+                                if ($carry == NULL) {
+                                    return $item['limit_timestamp'];
+                                }
+                                return max($carry, $item['limit_timestamp']);
+                            }
+                        );
+                        $unitsOfWork[] = $unitOfWork;
+                    }
+                }
+
+                foreach ($unitsOfWork as $unitOfWork) {
+                    $fromCount = count($unitOfWork['from']);
+                    $toCount = count($unitOfWork['to']);
+
+                    $nameContains = function ($array, $needle) {
+                        return array_reduce(
+                            $array,
+                            function ($carry, $item) use ($needle) {
+                                return $carry || strpos($item['name'], $needle) !== FALSE;
+                            },
+                            FALSE);
+                    };
+
+                    if ($nameContains($unitOfWork['from'], 'w_') && $nameContains($unitOfWork['to'], 'd_')) {
+                        $output->writeln('<error>Traveling backwards is not supported</error>');
+                        break;
+                    }
+                    if ($nameContains($unitOfWork['from'], 'd_') && $nameContains($unitOfWork['to'], 'w_')) {
+                        sleep(0); //TODO: reduce resolution due to day to week partition transition
                     }
 
-                    $sql .= "\n);";
+                    $fromPartsStr = substr(array_reduce(
+                        $unitOfWork['from'],
+                        function ($carry, $item) {
+                            return $carry . '`' . $item['name'] . '`, ';
+                        },
+                        ''
+                    ), 0, -2);
+                    $sql = "ALTER TABLE `${table}` REORGANIZE PARTITION ${fromPartsStr} INTO (\n";
 
-                    $output->writeln($sql);
+                    foreach ($unitOfWork['to'] as $step) {
+                        if (!$step['max']) {
+                            $sql .= "PARTITION `${step['name']}` VALUES LESS THAN (${step['limit_timestamp']}),\n";
+                        }
+                        else {
+                            $sql .= "PARTITION `${step['name']}` VALUES LESS THAN MAXVALUE,\n";
+                        }
+                    }
+                    $sql = substr($sql, 0, -2);
+
+                    $sql .= ');' . "\n";
+
+                    $output->write(print_r($sql, TRUE));
 
                     DB::getConn()->query($sql);
                 }
