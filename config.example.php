9d1e7cd73c86839b8176282b1b27143b6ad03845
Christian Kohlstedde <christian@kohlsted.de>
2018-10-15 13:12:32 +0200

    Add LICENSE and README. Now ready to publish…

---
+++ config.example.php
@@ -0,0 +1,19 @@
+<?php
+
+date_default_timezone_set('UTC');
+
+define('DB_HOST', 'host');
+define('DB_USER', 'user');
+define('DB_PASS', 'pass');
+define('DB_NAME', 'db');
+
+define('MIN_SLEEP_TIME', 10);
+define('MAX_SLEEP_TIME', 60);
+
+define('DISABLE_PLAYER', false);
+
+// define('HIGHSCORE_DAY_PAST', '7');
+// define('HIGHSCORE_DAY_FUTURE', '20');
+
+// Save every retrieved xml file to this path
+// define('CACHE_DIR', dirname(realpath(__FILE__)) . DIRECTORY_SEPARATOR . 'cache');
